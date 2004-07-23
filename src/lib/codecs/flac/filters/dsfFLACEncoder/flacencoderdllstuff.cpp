4795c9cbd85376d3a538628f2b9dc26a3522e4b4
Zentaro Kavanagh <ogg@illiminable.com>
2004-07-23 04:36:58 +0000

    * Added flac .def file and dll code.
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@7270 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ src/lib/codecs/flac/filters/dsfFLACEncoder/flacencoderdllstuff.cpp
@@ -0,0 +1,10 @@
+#include "StdAfx.h"
+#include ".\flacencoderdllstuff.h"
+
+flacencoderdllstuff::flacencoderdllstuff(void)
+{
+}
+
+flacencoderdllstuff::~flacencoderdllstuff(void)
+{
+}
---
+++ src/lib/codecs/flac/filters/dsfFLACEncoder/flacencoderdllstuff.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class flacencoderdllstuff
+{
+public:
+	flacencoderdllstuff(void);
+	~flacencoderdllstuff(void);
+};
