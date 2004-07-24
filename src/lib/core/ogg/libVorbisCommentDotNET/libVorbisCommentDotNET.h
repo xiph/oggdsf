3e92a093b10e6836a0366038f569e2c757c8ab1b
Zentaro Kavanagh <ogg@illiminable.com>
2004-07-24 07:32:12 +0000

    * Add skeleton .NET vorbis comment library.
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@7305 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ src/lib/core/ogg/libVorbisCommentDotNET/libVorbisCommentDotNET.h
@@ -0,0 +1,13 @@
+// libVorbisCommentDotNET.h
+
+#pragma once
+
+using namespace System;
+
+namespace libVorbisCommentDotNET
+{
+	public __gc class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
