95c94390bcbc56d14aca89122f683a4e3dc5a456
Zentaro Kavanagh <ogg@illiminable.com>
2004-10-21 04:39:33 +0000

    * Pre-release config.
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8059 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/lib/core/directshow/dsfOggDemux/OggDemuxSourcePin.h
+++ src/lib/core/directshow/dsfOggDemux/OggDemuxSourcePin.h
@@ -61,7 +61,7 @@ public:
 
 	virtual ~OggDemuxSourcePin(void);
 	static const unsigned long BUFFER_SIZE = 1024*512;			//What should this be ????
-	static const unsigned long NUM_BUFFERS = 100;
+	static const unsigned long NUM_BUFFERS = 75;
 
 
 	virtual bool deliverOggPacket(StampedOggPacket* inOggPacket);
