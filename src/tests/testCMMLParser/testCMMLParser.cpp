5b5e3f6d966c0ce4071970cf6febb130c1db3677
Zentaro Kavanagh <ogg@illiminable.com>
2004-11-30 08:13:54 +0000

    * Distribution project for CMML libs and tests to help fast testing turnaround.
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8309 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/tests/testCMMLParser/testCMMLParser.cpp
+++ src/tests/testCMMLParser/testCMMLParser.cpp
@@ -3,8 +3,30 @@
 
 #include "stdafx.h"
 
+#include "libCMMLTags.h"
+
+#include "libWinCMMLParse.h"
+#include "CMMLParser.h"
+
+
+bool testHeadParse(wstring inHeadString, C_HeadTag* outHeadTag) {
+	CMMLParser locParser;
+	return locParser.parseHeadTag(inHeadString, outHeadTag);
+
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	wstring head_1 = L"<head><title>Types of fish</title><meta name=\"Producer\" content=\"Joe Ordinary\"/><meta name=\"DC.Author\" content=\"Joe's friend\"/></head>";
+
+	wcout << "Original"<<endl<<head_1<<endl<<endl;
+
+	C_HeadTag locHead;
+	testHeadParse(head_1, &locHead);
+
+	cout<<"Parsed"<<endl<<locHead.toString()<<endl<<endl;
+
 	return 0;
 }
 
