c132fdd5273d8ea19f6f0380cfe1fa1e0c15b32e
Zentaro Kavanagh <ogg@illiminable.com>
2004-12-14 06:41:14 +0000

    * Start of the http seeking interface
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8383 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/lib/core/directshow/dsfOggDemux/HTTPOggSeeking.h
+++ src/lib/core/directshow/dsfOggDemux/HTTPOggSeeking.h
@@ -1,8 +1,22 @@
 #pragma once
 
 class HTTPOggSeeking
+	:	public AutoOggSeekTable
 {
 public:
 	HTTPOggSeeking(void);
-	~HTTPOggSeeking(void);
+	virtual ~HTTPOggSeeking(void);
+
+	virtual bool buildTable();
+
+	//IOggCallback interface
+	virtual bool acceptOggPage(OggPage* inOggPage);
+
+	__int64 fileDuration();
+
+	bool addSeekPoint(__int64 inTime, unsigned long mStartPos);
+	tSeekPair getStartPos(__int64 inTime);
+	
+
+	
 };
