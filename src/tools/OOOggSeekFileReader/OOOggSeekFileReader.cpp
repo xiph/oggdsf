683bfa245ec577955c152340216f15bde32bc473
Zentaro Kavanagh <ogg@illiminable.com>
2004-12-16 13:04:15 +0000

    * Seek file format reader.
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8443 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ src/tools/OOOggSeekFileReader/OOOggSeekFileReader.cpp
@@ -0,0 +1,45 @@
+// OOOggSeekFileReader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "illicoreconfig.h"
+#include "iLE_Math.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int __cdecl _tmain(int argc, _TCHAR* argv[])
+{
+
+	if (argc < 2) {
+		cout << "Usage : OOOggSeekFileReader <seek_table_file>"<<endl;
+
+	} else {
+		LOOG_INT64 timePoint;
+		unsigned long bytePos;
+
+		fstream seekFile;
+		seekFile.open(argv[1], ios_base::in | ios_base::binary);
+	
+		unsigned char* buff = new unsigned char[16];
+		unsigned long pointCount = 0;
+		while (!seekFile.eof()) {
+			//Read the time
+			seekFile.read((char*)buff, 8);
+			timePoint = iLE_Math::CharArrToInt64(buff);
+
+			//Read the byte offset
+			seekFile.read((char*)buff, 4);
+			bytePos = iLE_Math::charArrToULong(buff);
+
+			cout << "Seek point "<<pointCount<<" : Time = "<<timePoint<<", Byte Offset = "<<bytePos<<endl;
+
+
+		}
+
+		delete buff;
+	}
+	return 0;
+}
+
