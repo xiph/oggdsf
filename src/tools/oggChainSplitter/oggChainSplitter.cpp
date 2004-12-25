a70e691128204562da3fbe1dd249273b6854a4ad
André Pang <Andre.Pang@csiro.au>
2004-12-25 17:43:18 +0000

    oggdsf:  * Fix compilation of oggChainSplitter
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8537 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/tools/oggChainSplitter/oggChainSplitter.cpp
+++ src/tools/oggChainSplitter/oggChainSplitter.cpp
@@ -34,8 +34,8 @@
 
 #include "stdafx.h"
 
-#include <libOOOgg.h>
-#include <dllstuff.h>
+#include <libOOOgg/libOOOgg.h>
+#include <libOOOgg/dllstuff.h>
 #include "VorbisComments.h"
 #include <iostream>
 #include <fstream>
