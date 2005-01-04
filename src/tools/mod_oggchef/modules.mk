95cbbe8bf6814489aaa2afe7d10872bc3835a29c
André Pang <Andre.Pang@csiro.au>
2005-01-04 07:41:40 +0000

    oggdsf:  * Fix compilation of mod_oggchef on UNIX
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8604 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/tools/mod_oggchef/modules.mk
+++ src/tools/mod_oggchef/modules.mk
@@ -1,4 +1,4 @@
-mod_oggchef.la: mod_oggchef.slo
+mod_oggchef.la: mod_oggchef.slo anx_time.slo query_utils.slo
 	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_oggchef.lo
 DISTCLEAN_TARGETS = modules.mk
 shared =  mod_oggchef.la
