1d790cb673ecedd83654a6e3998bf8a494a1ab5f
André Pang <Andre.Pang@csiro.au>
2005-01-09 23:32:12 +0000

    oggdsf:  * Portability fixes for libCMMLParse
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@8706 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/lib/codecs/cmml/libCMMLParse/libCMMLParse.h
+++ src/lib/codecs/cmml/libCMMLParse/libCMMLParse.h
@@ -1,11 +1,13 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the LIBCMMLPARSE_EXPORTS
-// symbol defined on the command line. this symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// LIBCMMLPARSE_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
-#ifdef LIBCMMLPARSE_EXPORTS
-#define LIBCMMLPARSE_API __declspec(dllexport)
+#pragma once
+
+
+#ifdef WIN32
+# ifdef LIBCMMLPARSE_EXPORTS
+#  define LIBCMMLPARSE_API __declspec(dllexport)
+# else
+#  define LIBCMMLPARSE_API __declspec(dllimport)
+# endif
 #else
-#define LIBCMMLPARSE_API __declspec(dllimport)
+# define LIBCMMLPARSE_API
 #endif
+
