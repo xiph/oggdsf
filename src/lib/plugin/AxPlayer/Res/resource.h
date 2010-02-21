0055c506cab6fe3ec5ab643634129a37be31282e
Cristian Adam <cristian.adam@gmail.com>
2010-02-21 22:19:15 +0000

    Another file is missing.
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@16929 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ src/lib/plugin/AxPlayer/Res/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AxPlayer.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_AXPLAYER                    101
+#define IDR_VIDEOTAGBEHAVIOR            102
+#define IDR_HTMLMEDIAELEMENT            103
+#define IDR_MOUSEEVENTSINK              104
+#define IDR_HTMLEVENTSINK               105
+#define IDR_HTMLEVENTSINK1              107
+#define IDR_EVENTSINK                   110
+#define IDR_HTMLEVENTSSINK              111
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_SYMED_VALUE           112
+#endif
+#endif
