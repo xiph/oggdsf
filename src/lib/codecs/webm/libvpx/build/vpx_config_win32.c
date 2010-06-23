b3ea5e923c36768850d7f50d63931258f125d4fd
Cristian Adam <cristian.adam@gmail.com>
2010-06-23 20:51:02 +0000

    Updated libvpx to 0.9.1
    git-svn-id: https://svn.xiph.org/trunk/oggdsf@17304 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ src/lib/codecs/webm/libvpx/build/vpx_config_win32.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--target=x86-win32-vs8";
+const char *vpx_codec_build_config(void) {return cfg;}
---
+++ src/lib/codecs/webm/libvpx/build/vpx_config_x64.c
@@ -0,0 +1,2 @@
+static const char* const cfg = "--target=x64-win64-vs8";
+const char *vpx_codec_build_config(void) {return cfg;}
